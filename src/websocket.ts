ea147b17e24c51b1a93211256c9ce01072db9d71
Alexander Davis <alexander.davis.098@gmail.com>
2023-01-04 21:12:49 -0300

    refactor: refactor and adding events

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,56 +1,62 @@
 import { io } from "./http";
 
-interface User {
-    socketId: string;
-    name: string;
-}
+import { IUserModel, User } from "./models/User";
+import { Message } from "./models/Message";
 
-interface Message {
-    uid: string;
-    text: string;
-    createdAt: Date;
-    name: string;
-}
+import Logging from "./lib/logging";
 
-const users: User[] = [];
-const messages: Message[] = [];
+const EVENTS = {
+    connection: "connection",
+};
 
-io.on("connection", socket => {
-    console.log("[IO]: Connection => Server has a new connection");
+io.on(EVENTS.connection, socket => {
+    Logging.info(`[IO]: Connection => Server has a new connection.`);
+    Logging.info(`[IO]: Connection => User ${socket.id} has connected to the server!`);
 
-    socket.on("user", data => {
+    socket.on("user", async (data: IUserModel) => {
         console.log("[SOCKET]: User =>", data);
 
-        const user = users.find(user => user.name === data.name);
-
-        // If there is already a user I just change his socket id.
-        if (user)
-            user.socketId = socket.id;
-        else
-            users.push({
-                name: data.name,
-                socketId: socket.id
+        await User.findOne({ name: data.name })
+            .then(async (user) => {
+                // If there is already a user I just change his socket id.
+                if (user)
+                    user.socketId = socket.id;
+                else
+                    await User.create({ uuid: data.uuid, name: data.name, socketId: socket.id })
+                        .then(() => Logging.success("User successfully registered!"))
+                        .catch(err => Logging.error(err));
             })
+            .catch(err => Logging.error(err));
+        
+        // Searches to find out if the user logged into the external database is the same as the user logged into the socket. If so, I return this user.
+        await User.findOne({ uuid: data.uuid })
+            .then(async (user) => {
+                if (user)
+                    socket.emit("user", user.id);
+            })
+            .catch(err => Logging.error(err));
     });
 
-    socket.on("chat.message", data => {
+    socket.on("chat.message", async (data: any) => {
         console.log("[SOCKET]: Chat.message =>", data);
 
-        const message: Message = {
-            uid: data.uid,
-            name: data.name,
-            text: data.text,
-            createdAt: new Date()
-        }
-
-        messages.push(message);
-
-        console.log(messages);
-
-        io.emit("chat.message", message);
+        await User.findOne({ name: data.name })
+            .then(async (user) => {
+                if (user)
+                    await Message.create({ uid: data.uid, text: data.text, user: user.id })
+                        .then((newMessage) => io.emit("chat.message", newMessage))
+                        .catch(err => Logging.error(err));
+            })
+            .catch(err => Logging.error(err));
     });
 
+    // Send all messages
+    Message.find()
+        .then((messages) => io.emit("chat.all.messages", messages))
+        .catch(err => Logging.error(err));
+
     socket.on("disconnect", () => {
-        console.log("[SOCKET]: Disconnect => A connection was disconnected");
+        Logging.info("[SOCKET]: Disconnect => A connection was disconnected");
+        Logging.info(`[SOCKET]: Disconnect => User ${socket.id} has disconnected from the server!`);
     })
 });
