675121e3d0d3b63a7bb3227ef313378299712c30
Alexander Davis <alexander.davis.098@gmail.com>
2023-01-04 21:10:08 -0300

    feat: added configs

---
+++ src/config/index.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_DATABASE = process.env.MONGO_DATABASE || "";
+const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_DATABASE}.iku7e6u.mongodb.net/test`;
+
+const PORT = process.env.PORT || 3335;
+
+const IO_CORS_ORIGIN = "http://localhost:3000";
+
+export const config = {
+    mongo: {
+        uri: MONGO_URI
+    },
+    server: {
+        port: PORT
+    },
+    io: {
+        origin: IO_CORS_ORIGIN
+    }
+}
