26ee144cfa5c4e78847538f1bf09df26865ff447
Alexander Davis <alexander.davis.098@gmail.com>
2023-09-16 23:45:35 -0300

    ✨feat: added new online user event

--- src/websocket.ts
+++ src/websocket.ts
@@ -9,10 +9,27 @@ import Logging from "./lib/logging";
 
 const EVENTS = config.socket.events;
 
+const onlineUsers = new Map(); // Map para mapear os IDs dos usuários para seus sockets
+
 io.on(EVENTS.connection, (socket) => {
-    Logging.info(`[IO]: Connection => User ${socket.id} has connected to the server!`);
+    Logging.info("[IO]: A new connection has been established.");
 
     socket.on(EVENTS.user, async (data: IUserModel) => {
+        Logging.info(`[SOCKET]: Connection => User ${socket.id} has connected to the server!`);
+
+        // Try to find the user in the list of online users by uuid
+        const existingSocket = onlineUsers.get(data.uuid);
+
+        if (existingSocket)
+            // If the user is already online, you can update the associated socket
+            existingSocket.socketId = socket.id;
+        else
+            // If the user is not online, create a new record on the map
+            onlineUsers.set(data.uuid, {
+                socketId: socket.id,
+                userId: data.uuid,
+            });
+
         await User.findOne({ name: data.name })
             .then(async (user) => {
                 // If there is already a user I just change his socket id.
@@ -23,7 +40,11 @@ io.on(EVENTS.connection, (socket) => {
                         .then(() => Logging.success("User successfully registered!"))
                         .catch(err => Logging.error(err));
             })
-            .catch(err => Logging.error(err));
+            .catch(err => Logging.error(err))
+            .finally(() => {
+                socket.emit(EVENTS.onlineUsers, onlineUsers.size);
+                Logging.info(`[SOCKET]: Online users: ${onlineUsers.size}`);
+            });
 
         // Searches to find out if the user logged into the external database is the same as the user logged into the socket. If so, I return this user.
         await User.findOne({ uuid: data.uuid })
@@ -54,6 +75,13 @@ io.on(EVENTS.connection, (socket) => {
 
     // Disconnect event
     socket.on(EVENTS.disconnect, async () => {
-        Logging.info(`[SOCKET]: Disconnect => User ${socket.id} has disconnected from the server!`);
+        // Find the user record by socketId and remove it from map
+        for (const [uuid, user] of onlineUsers.entries()) {
+            if (user.socketId === socket.id) {
+                onlineUsers.delete(uuid);
+                Logging.info(`[SOCKET]: Disconnect => User ${socket.id} has disconnected from the server!`);
+                break;
+            }
+        }
     })
 });
